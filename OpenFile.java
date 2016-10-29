d9e94705caac9bda874567496a9cbeed756e8e2e
Vishal thakkar <thakkarvishal96@users.noreply.github.com>
2016-10-29 14:49:31 +0530

    Add files via upload

---
+++ OpenFile.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+ 
+class Notepad {
+  public static void main(String[] args) {
+    Runtime rs = Runtime.getRuntime();
+ 
+    try {
+      rs.exec("notepad");
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }   
+  }
+}
